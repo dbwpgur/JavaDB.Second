1f8c2e7797c5a7b7af34a5003afc462af119cf20
Unknown <dbwpgur@naver.com>
2018-07-04 17:03:57 +0900

    Switch 연습예제2

---
+++ src/day2/SwitchEx2.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class SwitchEx2 {
+
+	public static void main(String[] args) {
+	char ch = 'C';
+		
+		switch(ch){
+		case 'a':
+			System.out.println("a 입니다");
+			break;
+		case 'A':
+			System.out.println("A 입니다");
+			break;
+		case 'b':
+			System.out.println("b 입니다");
+			break;
+		case 'B':
+			System.out.println("B 입니다");
+			break;
+		default:
+			System.out.println("A,a,B,b 가 아닙니다");
+		}
+		
+	}
+
+}
