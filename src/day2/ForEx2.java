02e9ea46c38ce72bc1725729430d6b03dfe1dcd2
Unknown <dbwpgur@naver.com>
2018-07-04 21:17:23 +0900

    for문 예제 2

---
+++ src/day2/ForEx2.java
@@ -0,0 +1,34 @@
+package day2;
+
+public class ForEx2 {
+
+	public static void main(String[] args) {
+		int i =0;
+		int sum =1;
+		/*
+		for(i=1; i<=10; i++){
+			if((i % 2)==1) {
+				System.out.println( i +"는 홀수입니다");
+			}
+		
+		
+		}
+		for(i=1; i<=10; i+=2){
+			System.out.println( i );
+		}
+		
+		
+		for(i=1; i<=10; i++){
+		   sum=sum+i;
+		
+		}
+		System.out.println(sum);
+		*/
+		int num =7;
+		
+		for(i=1; i<=10; i++){
+        System.out.println("7" + "*"+i +"=" +i*7);
+		}
+	}
+
+}
