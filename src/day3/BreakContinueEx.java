b32be0211b3e3b2a66dc01e39eeaf8e357781529
Unknown <dbwpgur@naver.com>
2018-07-11 17:41:52 +0900

    3일차 breakcontinue

---
+++ src/day3/BreakContinueEx.java
@@ -0,0 +1,43 @@
+package day3;
+
+public class BreakContinueEx {
+
+	public static void main(String[] args) {
+		for(int j=1; j<=5; j++){
+			
+		
+		for(int i=1; i<=5; i++){
+			System.out.println("j="+j+",i="+i);
+			if(i==4){
+				break;
+			}
+		}
+		}
+	
+	
+//		for(int j=1; j<=5; j++){
+//			
+//			
+//			for(int i=1; i<=5; i++){
+//		
+//				if(i==4){
+//					break;
+//				}
+//				System.out.println("j="+j+",i="+i);
+//			}
+//			}
+//	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
