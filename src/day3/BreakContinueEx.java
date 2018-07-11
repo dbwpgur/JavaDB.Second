f03e8b5febb8d10d9d498cbb0125b35c7c84b34b
Unknown <dbwpgur@naver.com>
2018-07-11 18:54:05 +0900

    3일차 continue

--- src/day3/BreakContinueEx.java
+++ src/day3/BreakContinueEx.java
@@ -3,18 +3,34 @@ package day3;
 public class BreakContinueEx {
 
 	public static void main(String[] args) {
-		for(int j=1; j<=5; j++){
+		
 			
 		
-		for(int i=1; i<=5; i++){
-			System.out.println("j="+j+",i="+i);
-			if(i==4){
-				break;
-			}
-		}
-		}
+//		for(int i=1; i<=5; i++){
+//			System.out.println("j="+j+",i="+i);
+//			if(i==4){
+//				break;
+//			}
+//		}
+//		}
+//이름이 있는 포문예제	   
+//			TEST:
+//		for(int i=1; i<=5; i++){
+//			System.out.println("j="+j+",i="+i);
+//			if(i==4){
+//				break TEST;
+//			}
+//		}
+//		}
 	
-	
+
+		
+		
+		
+		
+		
+		
+		
 //		for(int j=1; j<=5; j++){
 //			
 //			
@@ -28,7 +44,20 @@ public class BreakContinueEx {
 //			}
 //	
 	
-	
+
+		
+		
+	int num=0;
+	for(int i=1;i<=100;i++){
+		
+		if(i%2==1)
+			continue;
+		num=num+i;
+		
+	}
+	System.out.println(num);
+	}
+     
 	
 	
 	
@@ -40,4 +69,4 @@ public class BreakContinueEx {
 	
 	}
 
-}
+
